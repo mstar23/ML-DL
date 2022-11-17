b015a109d016eda230e22610eb95ab23b2f3ea67
mstar23 <mjy3056@naver.com>
2022-11-17 18:54:26 +0900

    add proj name

--- LGBM_APP.py
+++ LGBM_APP.py
@@ -7,7 +7,11 @@ def get_image(image_name):
     image = Image.open(image_path) # 경로와 확장자 주의!
     st.image(image)
 
+#### 프로젝트 네임 #####
+st.write('# LGBM 실습 페이지')
 import pandas as pd # 판다스 불러오기
 data_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/fraud.csv'
 df = pd.read_csv(data_url) # URL로 CSV 불러오기
+st.write('### 사용한 데이터')
 st.write(df.head())
+
