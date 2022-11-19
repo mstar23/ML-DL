0426a3c688290b116dc6c52d682e91272c320b62
Mstar <mjy3056@naver.com>
2022-11-19 22:04:39 +0900

    휴

--- LGBM_APP.py
+++ LGBM_APP.py
@@ -14,11 +14,11 @@ st.write('# LGBM 실습 페이지')
 st.write('집에서 수정중')
 #### 데이터 불러오기 ####
 st.write('#### 전처리한 데이터')
-st.write('import 문제가 해결이 왜 안되지?')
-pkl_path = f"{os.path.dirname(os.path.abspath(__file__))}/lgbm_df.pkl"
-lgbm_df = joblib.load(pkl_path)
-lgbm_df = pd.DataFrame(lgbm_df)
-st.write(lgbm_df.head())
+st.write('큰일 날뻔 ㅎ')
+# pkl_path = f"{os.path.dirname(os.path.abspath(__file__))}/lgbm_df.pkl"
+# lgbm_df = joblib.load(pkl_path)
+# lgbm_df = pd.DataFrame(lgbm_df)
+# st.write(lgbm_df.head())
 
 ### 불러온 데이터로 훈련-테스트셋 분리
 
