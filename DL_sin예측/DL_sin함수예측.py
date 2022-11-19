c7e2cc48f838bd3cac46a64401ed13730819cfed
Mstar <mjy3056@naver.com>
2022-11-19 16:02:46 +0900

    colab 주소 추가

--- DL_sin예측/DL_sin함수예측.py
+++ DL_sin예측/DL_sin함수예측.py
@@ -1,5 +1,15 @@
 import streamlit as st
 import joblib
 import numpy as np
+import pandas as pd
+import math
+# import torch
+import matplotlib.pyplot as plt
 
 st.write('## sin함수 예측하기')
+st.write(
+    '''
+    colab 노트북 주소 : https://colab.research.google.com/drive/1NNg3VC674PrIFBVcYJcVepeWGZc6JkEe?usp=sharing
+    '''
+)
+
