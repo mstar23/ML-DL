d45be02a9ff0e40f5f2f2e67def99f71a85e4cc3
Mstar <mjy3056@naver.com>
2022-11-27 22:44:04 +0900

    app수정

--- Mini_proj2/app.py
+++ Mini_proj2/app.py
@@ -11,8 +11,10 @@ st.write('##  랜덤포레스트를 이용한 비행기 가격 예측')
 st.write('---')
 
 # 데이터 불러오기
+st.subheader('**이용할 data 상위 5개**')
 data_path = f"{os.path.dirname(os.path.abspath(__file__))}/data.csv"
 data = pd.read_csv(data_path)
 df = pd.DataFrame(data)
 df.drop('Unnamed: 0',axis=1,inplace=True)
 st.write(df.head())
+
