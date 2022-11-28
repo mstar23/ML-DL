1e0dee6156415d484bc93faa6a088b4b4d764a6e
mstar23 <mjy3056@naver.com>
2022-11-28 12:19:40 +0900

    streamlit

--- Mini_proj2/app.py
+++ Mini_proj2/app.py
@@ -240,13 +240,16 @@ elif options == '03. 시각화(plotly)':
        test_relation_square = model.score(X_test, y_test)
        st.write(f' train 결정계수 : {train_relation_square}, test 결정계수 : {test_relation_square}')
 
-        # 시각화 해보기
-       fig = make_subplots(rows=1, cols=1, shared_xaxes=True)
-       fig.add_trace(go.Scatter(x=y_train,y=y_test, mode='markers',name='Train'))
-       fig.add_trace(go.Scatter(x=y_test,y=test_pred,mode='markers',
-                    name='Test')) # mode='lines+markers'
-       fig.update_layout(title='<b>actual과 predict 비교')
-       st.plotly_chart(fig)
+       SearchBtn = st.button('Search')
+
+       if SearchBtn:
+           # 시각화 해보기
+        fig = make_subplots(rows=1, cols=1, shared_xaxes=True)
+        fig.add_trace(go.Scatter(x=y_train,y=y_test, mode='markers',name='Train'))
+        fig.add_trace(go.Scatter(x=y_test,y=test_pred,mode='markers',
+                        name='Test')) # mode='lines+markers'
+        fig.update_layout(title='<b>actual과 predict 비교')
+        st.plotly_chart(fig)
        
 
     #### Tab3
