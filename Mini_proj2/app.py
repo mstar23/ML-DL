b147925ab6f324243ba047627463358c440c5046
mstar23 <mjy3056@naver.com>
2022-11-28 12:17:09 +0900

    streamlit

--- Mini_proj2/app.py
+++ Mini_proj2/app.py
@@ -54,9 +54,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
 import plotly.figure_factory as ff
-# import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt 
+import os
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 import time
 from PIL import Image     # 이미지 처리 라이브러리
@@ -213,6 +219,35 @@ elif options == '03. 시각화(plotly)':
     with tab_RF:
        st.header("RandomForest")
        st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+       data_path = f"{os.path.dirname(os.path.abspath(__file__))}/data.csv"
+       data = pd.read_csv(data_path)
+       df = pd.DataFrame(data)
+       df.drop('Unnamed: 0',axis=1,inplace=True)
+        # 데이터 전처리
+       X = df.drop('Price', axis=1)
+       y = df.Price
+       X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=100)
+
+        # score 와 mse 비교
+       model_pkl_path = f"{os.path.dirname(os.path.abspath(__file__))}/randomforest.pkl"
+       model = joblib.load(model_pkl_path)
+       st.write(model.score(X_train, y_train),model.score(X_test, y_test))
+       st.subheader('RMSE 비교')
+       train_pred = model.predict(X_train) 
+       test_pred = model.predict(X_test) 
+       train_relation_square = model.score(X_train, y_train)
+       test_relation_square = model.score(X_test, y_test)
+       st.write(f' train 결정계수 : {train_relation_square}, test 결정계수 : {test_relation_square}')
+
+        # 시각화 해보기
+       fig = make_subplots(rows=1, cols=1, shared_xaxes=True)
+       fig.add_trace(go.Scatter(x=y_train,y=y_test, mode='markers',symbol='pentagon',name='Train'))
+       fig.add_trace(go.Scatter(x=y_test,y=test_pred,mode='markers',symbol='circle',
+                    name='Test')) # mode='lines+markers'
+       fig.update_layout(title='<b>actual과 predict 비교')
+       st.plotly_chart(fig)
+       
 
     #### Tab3
     with tab_XGB:
