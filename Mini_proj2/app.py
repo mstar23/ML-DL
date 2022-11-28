d69331bd936f85425d15f0a18385845b3e559066
mstar23 <mjy3056@naver.com>
2022-11-28 16:32:49 +0900

    a

--- Mini_proj2/app.py
+++ Mini_proj2/app.py
@@ -262,7 +262,7 @@ elif options == '03. 시각화(plotly)':
        st.write(f' train 결정계수 : {train_relation_square}, test 결정계수 : {test_relation_square}')
 
        st.subheader('시각화 부분')
-       SearchBtn2 = st.button('Search')
+       SearchBtn2 = st.button('Show')
 
        if SearchBtn2:
            # 시각화 해보기
